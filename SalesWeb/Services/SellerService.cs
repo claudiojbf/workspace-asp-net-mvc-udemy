9d27c5505eca6f38258df8fd9240e30103f9bf2f
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-12-15 14:46:56 -0300

    Delete Seller

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -26,5 +26,17 @@ namespace SalesWeb.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Sellers.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Sellers.Find(id);
+            _context.Sellers.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
