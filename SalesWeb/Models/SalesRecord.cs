e6e7c419a48c530ab5d713b47c4f14b6f6075c6f
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-12-19 10:24:48 -0300

    Implementando grouping search

--- SalesWeb/Models/SalesRecord.cs
+++ SalesWeb/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using SalesWeb.Models.enuns;
 
 namespace SalesWeb.Models
@@ -6,7 +7,11 @@ namespace SalesWeb.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SalesStatus Status{ get; set; }
         public Seller Seller { get; set; }
--- SalesWeb/Services/SalesRecordService.cs
+++ SalesWeb/Services/SalesRecordService.cs
@@ -33,5 +33,24 @@ namespace SalesWeb.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Departament, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
